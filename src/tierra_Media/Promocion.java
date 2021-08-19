6f85b76a5c4f30b85befeaa09dac618831dde3bd
Polandia94 <pablo22estevez@gmail.com>
2021-08-19 16:32:06 -0300

    Primer Intento de Diagrama de Clases

---
+++ src/tierra_Media/Promocion.java
@@ -0,0 +1,9 @@
+package tierra_Media;
+
+public abstract class Promocion {
+	Atraccion[] atracciones;
+	int costoDeVisita;
+	public Promocion() {
+		
+	}
+}
---
+++ src/tierra_Media/PromocionCostoFinal.java
@@ -0,0 +1,15 @@
+package tierra_Media;
+
+public class PromocionCostoFinal extends Promocion {
+
+	
+	public PromocionCostoFinal(Atraccion[] atracciones, int costoDeVisita) {
+		super();
+		this.atracciones = atracciones;
+		this.costoDeVisita = costoDeVisita;
+	}
+	
+
+
+
+}
